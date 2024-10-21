a62df6a46764b86528cba72fef74627ff25ccd2e
Katoedi <katoedi03@gmail.com>
2024-10-21 21:33:00 +0300

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,10 +1,11 @@
 stire = "În contextul comparativ, chiar dacă este vorba de o performanţă uşor mai bună decât creşterea de 4,5% aşteptată de analişti, totuşi este mai puţin decât avansul de 4,7% înregistrat în trimestrul al doilea al acestui an. Şi mai important, aceasta cea mai slabă performanţă economică înregistrată de la începutul lui 2023, când China tocmai pusese capăt politicii sanitare ‘zero Covid’ care a paralizat deplasările şi consumul, ceea ce a afectat şi activitatea economică."
 print(len(stire))
 stire_1 = (stire[0:235:])
-stire_2 = (stire[235::])
+stire_2 = (stire[235::-1])
 stire_1 = (stire_1.upper())
 stire_1 = stire_1.strip()
-print(stire_1)
-sitre_2 = (stire_2[469:235:-1])
-stire_2 = (stire_2.upper())
+stire_2 = stire_2.translate(str.maketrans("", "", ".,!"))
+stire = stire_1 + stire_2
+print(stire)
+
 
