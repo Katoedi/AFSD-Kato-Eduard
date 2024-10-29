dcb5495b4acb2e88aea021186aa586080000b825
Katoedi <katoedi03@gmail.com>
2024-10-29 21:34:19 +0200

    tema3

--- tema3.py
+++ tema3.py
@@ -6,17 +6,53 @@ tavi = ["tava"] * 7  # stiva LIFO
 
 
 istoric_comenzi = []
-contor_comenzi = [0, 0, 0]
+contor_comenzi = []
 while studenti and comenzi:
     student = studenti.pop()
     comanda = comenzi.pop()
     if student and comanda:
         print(f'{student} a comandat {comanda}.')
         tavi.pop()
-        istoric_comenzi.append(comanda)
-
-        #contor_comenzi.append
+        istoric_comenzi.append((student, comanda))
+        contor_comenzi.append(comanda)
 print(istoric_comenzi)
-meniu = meniu.remove(istoric_comenzi)
+meniu.remove(comanda)
 print(meniu)
+print(f""" 
+S-au comandat {comanda.count('papanasi')} papanasi.
+S-au comandat {comanda.count('guias')} guiasuri.
+S-au comandat {comanda.count('ceafa')} cefe.
+          """)
+print(f'Mai exista {len(tavi)} tavi disponibile.')
+if 'papanasi' in meniu:
+    print("Mai sunt papanasi disponibili.")
+else:
+    print("Nu mai sunt papanasi disponibili.")
+
+if 'ceafa' in meniu:
+    print("Mai este ceafa disponibilă.")
+else:
+    print("Nu mai este ceafa disponibilă.")
+
+if 'guias' in meniu:
+    print("Mai este guias disponibil.")
+else:
+    print("Nu mai este guias disponibil.")
+pret = 0
+for x in contor_comenzi:
+    if x == 'papanasi':
+        pret += 7
+    elif x == 'ceafa':
+        pret += 10
+    elif x == 'guias':
+        pret += 5
+print(f"Cantina a incasa {pret} de lei. ")
+produse_7lei = []
+for i in preturi:
+    nume_produs = i[0]
+    pret = i[1]
+    if pret <= 7:
+        produse_7lei.append((nume_produs, pret))
+print(f'Produsele sub 7 lei sunt: {produse_7lei}')
+
 
